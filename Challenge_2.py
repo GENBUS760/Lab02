51f8f8922eb4b1b51600520b95033f18ce02f98e
Emma Dong <cdong35@wisc.edu>
2021-09-21 23:47:26 -0500

    Challenge_2

--- Challenge_2.py
+++ Challenge_2.py
@@ -2,4 +2,4 @@ import boto3
 s3 = boto3.resource('s3')
 s3.create_bucket(Bucket='mybucket', CreateBucketConfiguration={
     'LocationConstraint': 'us-west-1'})
-# code from Stackoverflow
+# code from Stackoverflow (https://stackoverflow.com/questions/31092056/how-to-create-a-s3-bucket-using-boto3)
