f3a370c9045822fafa01f9b17660d5a47f079146
Ruolin <rzhang395@wisc.edu>
2021-09-21 20:24:04 -0500

    lab2

--- 05_github.py
+++ 05_github.py
@@ -4,13 +4,24 @@ import json # to parse JSON
 import requests # to make HTTP requests
 import argparse # to parse command-line arguments
 import time
-
 if __name__ == "__main__":
     url_1 = "https://api.github.com/emaadmanzoor"
     r = requests.get(url_1)
-    print(r.status_code)
-    print(r.json())
-    
+    attempts=0
+    while attempts<10:
+    	r=requests.get(url_1)
+    	if r.status_code==200:
+    	    print(r.status_code)
+    	    print(r.json())
+    	    break
+    	attempts = attempts+1
+    	print('failed')
+    	time.sleep(5)
+    	if attempts == 10:
+    	    print('error')
+    	    break
+   	   
+'''    
     url_2 = "https://api.github.com/users/emaadmanzoor"
     r = requests.get(url_2)
     print(r.status_code)
@@ -34,3 +45,4 @@ if __name__ == "__main__":
 
     print("Sleeping until reset...")
     time.sleep(remaining_time)
+ '''
