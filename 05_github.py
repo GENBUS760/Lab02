08577b35ea9c1837b15869e02327b531075eb256
Nancy TAI <ntai3@wisc.edu>
2021-09-21 21:31:06 -0500

    lab 2

--- 05_github.py
+++ 05_github.py
@@ -7,23 +7,25 @@ import time
 
 if __name__ == "__main__":
     url_1 = "https://api.github.com/emaadmanzoor"
-    r1 = requests.get(url_1)
-    #print(r.status_code)
-    #print(r.json())
+    r = requests.get(url_1)
+    for x in range(1, r.status_code + 1):
+    	break
+    	print(r.status_code)
+    	print(r.json())
     
     url_2 = "https://api.github.com/users/emaadmanzoor"
-    r2 = requests.get(url_2)
-    print(r2.status_code)
+    r = requests.get(url_2)
+    print(r.status_code)
 
-    for key, value in r2.json().items():
+    for key, value in r.json().items():
         print(key, value)
 
-    print(r2.headers)
+    print(r.headers)
 
-    reset_time = r1.headers["x-ratelimit-reset"]
+    reset_time = r.headers["x-ratelimit-reset"]
     print("Rate limit resets at:", reset_time)
 
-    remaining = r1.headers["x-ratelimit-remaining"]
+    remaining = r.headers["x-ratelimit-remaining"]
     print("API calls remaining:", remaining)
 
     current_time = time.time()
