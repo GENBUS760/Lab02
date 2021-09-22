efe7567816cf511cb0700549ab5ae67010277edc
Yiyang Wang <wang2687@wisc.edu>
2021-09-21 20:01:47 -0500

    lab 2

--- 05_github.py
+++ 05_github.py
@@ -6,31 +6,38 @@ import argparse # to parse command-line arguments
 import time
 
 if __name__ == "__main__":
-    url_1 = "https://api.github.com/emaadmanzoor"
-    r = requests.get(url_1)
-    print(r.status_code)
-    print(r.json())
+    url_1 = "https://api.github.com/users/EliezerWang"
+    temp_r = requests.get(url_1)
+    if temp_r.status_code != 404:
+    	r = requests.get(url_1)
+    	print(r.status_code)
+    	print(r.json())
     
-    url_2 = "https://api.github.com/users/emaadmanzoor"
-    r = requests.get(url_2)
-    print(r.status_code)
+    url_2 = "https://api.github.com/users/ElizerWang"
+    temp_r = requests.get(url_2)
+    if temp_r.status_code != 404:
+    	r = requests.get(url_2)
+    	print(r.status_code)
 
-    for key, value in r.json().items():
-        print(key, value)
+    try:
+	    for key, value in r.json().items():
+		print(key, value)
 
-    print(r.headers)
+	    print(r.headers)
 
-    reset_time = r.headers["x-ratelimit-reset"]
-    print("Rate limit resets at:", reset_time)
+	    reset_time = r.headers["x-ratelimit-reset"]
+	    print("Rate limit resets at:", reset_time)
 
-    remaining = r.headers["x-ratelimit-remaining"]
-    print("API calls remaining:", remaining)
+	    remaining = r.headers["x-ratelimit-remaining"]
+	    print("API calls remaining:", remaining)
 
-    current_time = time.time()
-    reset_time = int(reset_time) # the API gives you a string, not an integer
-    remaining_time = reset_time - current_time
-    print("Remaining time (seconds)",
-            remaining_time)
+	    current_time = time.time()
+	    reset_time = int(reset_time) # the API gives you a string, not an integer
+	    remaining_time = reset_time - current_time
+	    print("Remaining time (seconds)",
+		    remaining_time)
 
-    print("Sleeping until reset...")
-    time.sleep(remaining_time)
+	    print("Sleeping until reset...")
+	    time.sleep(remaining_time)
+    except:
+    	print('There is no valid url')
