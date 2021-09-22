3372e8f7dc128178eee83e4c5f546a2a16fbc108
sandra239 <slu239@wisc.edu>
2021-09-21 20:08:15 -0500

    05_github.py

--- 05_github.py
+++ 05_github.py
@@ -8,29 +8,15 @@ import time
 if __name__ == "__main__":
     url_1 = "https://api.github.com/emaadmanzoor"
     r = requests.get(url_1)
-    print(r.status_code)
-    print(r.json())
     
-    url_2 = "https://api.github.com/users/emaadmanzoor"
-    r = requests.get(url_2)
-    print(r.status_code)
+    for i in range(5):
+    	s_code = r.status_code
+    	if s_code == 404:
+    		time.sleep(2)
+    		i = i+1
+    		print("this is an invalid URL, returns status code 404")
+    	else:
+    		print(r)
+    
 
-    for key, value in r.json().items():
-        print(key, value)
-
-    print(r.headers)
-
-    reset_time = r.headers["x-ratelimit-reset"]
-    print("Rate limit resets at:", reset_time)
-
-    remaining = r.headers["x-ratelimit-remaining"]
-    print("API calls remaining:", remaining)
-
-    current_time = time.time()
-    reset_time = int(reset_time) # the API gives you a string, not an integer
-    remaining_time = reset_time - current_time
-    print("Remaining time (seconds)",
-            remaining_time)
-
-    print("Sleeping until reset...")
-    time.sleep(remaining_time)
+   
