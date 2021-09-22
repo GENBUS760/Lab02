fc481d7fb9c8dab50b11f2e61e382a7e16e3b919
FangshuoLiu <fliu239@wisc.edu>
2021-09-21 20:45:16 -0500

    updat 05_github

--- 05_github.py
+++ 05_github.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python
 
-import json # to parse JSON
-import requests # to make HTTP requests
-import argparse # to parse command-line arguments
+import requests
 import time
 
-if __name__ == "__main__":
-    url_1 = "https://api.github.com/emaadmanzoor"
-    r = requests.get(url_1)
-    print(r.status_code)
-    print(r.json())
-    
-    url_2 = "https://api.github.com/users/emaadmanzoor"
-    r = requests.get(url_2)
-    print(r.status_code)
+url_1= "https://api.github.com/emaadmanzoor"
+rounds = 0
 
-    for key, value in r.json().items():
-        print(key, value)
+while rounds < 10:
+  r = requests.get(url_1)
+  stacode = r.status_code
+  if stacode == 403:
+  	print("the status code is", r.status_code)
+  	break
+  else:
+  	rounds = rounds + 1
+  	print ("Tried", rounds, "rounds and the status code is", stacode) 
 
-    print(r.headers)
-
-    reset_time = r.headers["x-ratelimit-reset"]
-    print("Rate limit resets at:", reset_time)
-
-    remaining = r.headers["x-ratelimit-remaining"]
-    print("API calls remaining:", remaining)
-
-    current_time = time.time()
-    reset_time = int(reset_time) # the API gives you a string, not an integer
-    remaining_time = reset_time - current_time
-    print("Remaining time (seconds)",
-            remaining_time)
-
-    print("Sleeping until reset...")
-    time.sleep(remaining_time)
+time.sleep(1) 
+print ("This is an invalid URL which return status code 404 after 10-round trial")
