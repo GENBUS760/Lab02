b693cdeff7f058af4698e2044b4c7e432bf0f22f
Alice Shen <shen258@wisc.edu>
2021-09-23 19:43:47 -0500

    removed personal credential. Added user input

--- aws.py
+++ aws.py
@@ -1,13 +1,21 @@
 import boto3
 
+'''
+Edited 9/23
+- Removed Personal Credential
+- Added user input (key_id, secret_key)
+'''
+
+key_id = input('Please enter your aws_access_key_id:')
+secret_key = input('Please enter your aws_secret_access_key:')
+
 session = boto3.Session(
-    aws_access_key_id='AKIAVUY4Z73H2LF54INP',
-    aws_secret_access_key='XWIzmdRLZSoLF+MXDV8PLcdtQzZqf490QycGZg+b'
+    aws_access_key_id=key_id,
+    aws_secret_access_key=secret_key
 )
 
 s3 = session.resource('s3')
 s3.create_bucket(Bucket='gb760lab02bucket')
 txt_data = b'extra credit?'
 object = s3.Object('gb760lab02bucket', 'hello.txt')
-result = object.put(Body=txt_data)
-
+object.put(Body=txt_data)
